5a400eab8937a98dbf2590e8cef1cba9f1328fdd
Yousef <gheibiyousef2000@gmail.com>
2022-03-15 09:06:52 +0330

    implement responsive

--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,18 @@ const config = {
 };
 
 const myChart = new Chart(document.getElementById("chart"), config);
+
+
+
+// show or hide sidebar
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("aside");
+
+menuBtn.addEventListener('click',()=>{
+  sidebar.style.display ="block";
+})
+
+closeBtn.addEventListener("click", () => {
+  sidebar.style.display = "none";
+});
