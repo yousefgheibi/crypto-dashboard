af99c6dbdb739de87fbac64f8e49e58b30505dee
Yousef <gheibiyousef2000@gmail.com>
2022-03-15 09:15:59 +0330

    add dark-theme

--- assets/main.js
+++ assets/main.js
@@ -42,3 +42,17 @@ menuBtn.addEventListener('click',()=>{
 closeBtn.addEventListener("click", () => {
   sidebar.style.display = "none";
 });
+
+
+
+// theme-toggle
+const themeBtn = document.querySelector(".theme-btn");
+
+themeBtn.addEventListener("click",()=>{
+  document.body.classList.toggle("dark-theme");
+
+
+  themeBtn.querySelector("span:first-child").classList.toggle("active");
+  themeBtn.querySelector("span:last-child").classList.toggle("active");
+
+});
